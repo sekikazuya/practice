e517994ec237e7fa37a977aac4cd21daf7ae0ffc
sekikazuya <88357511+sekikazuya@users.noreply.github.com>
2021-11-25 14:41:23 +0900

    componentDidUpdateの練習

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -10,12 +10,27 @@ class Blog extends React.Component {
     }
   }
 
+  componentDidMount(){
+    // ボタンがクリックされたらいいねをカウントアップする
+    document.getElementById("counter").addEventListener('click', this.countUp)
+  }
+
+  componentDidUpdate(){
+    if (this.state.count >= 10) {
+        this.setState({ count: 0})
+    }
+  }
+
   // 公開状態を反転させる関数
   togglePublished = () => {
     this.setState( {
       isPublished: !this.state.isPublished
     })
   };
+
+  countUp = () => {
+    this.setState( { count: this.state.count + 1 })
+  }
 
   render() {
     return (
