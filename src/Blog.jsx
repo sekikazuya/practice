15440054373930c6e46d2ddc2b082bd9059f0e49
sekikazuya <88357511+sekikazuya@users.noreply.github.com>
2021-11-25 16:18:02 +0900

    React Hooksに書き換え

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -3,57 +3,40 @@ import Article from './Article';
 import * as FooBar from './components/FooBar';
 import Hoge from './components/Hoge';
 
-class Blog extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isPublished: false,
-      count: 0
-    }
-  }
+const Blog = () => {
 
-  componentDidMount(){
+
+  // componentDidMount(){
     // ボタンがクリックされたらいいねをカウントアップする
-    document.getElementById("counter").addEventListener('click', this.countUp)
-  }
+  //  document.getElementById("counter").addEventListener('click', this.countUp)
+  // }
 
-  componentDidUpdate(){
+  // componentDidUpdate(){
     // カウントが10になったら0に戻る
-    if (this.state.count >= 10) {
-        this.setState({ count: 0})
-    }
-  }
+  // if (this.state.count >= 10) {
+  //      this.setState({ count: 0})
+  //  }
+  // }
 
-  componentWillUnmount(){
-    document.getElementById("counter").removeEventListener('click', this.countUp)
-  }
+  // componentWillUnmount(){
+  //   document.getElementById("counter").removeEventListener('click', this.countUp)
+  // }
 
-  // 公開状態を反転させる関数
-  togglePublished = () => {
-    this.setState( {
-      isPublished: !this.state.isPublished
-    })
-  };
+  // countUp = () => {
+  //   this.setState( { count: this.state.count + 1 })
+  // }
 
-  countUp = () => {
-    this.setState( { count: this.state.count + 1 })
-  }
-
-  render() {
-    return (
-      <>
-        <Article 
-          title={"Reactの使い方"} 
-          isPublished={this.state.isPublished} 
-          toggle={() => this.togglePublished()}
-          count={this.state.count}
-        />
-        <FooBar.Foo />
-        <FooBar.Bar />
-        <Hoge />
-      </>
-    )
-  }
+  return (
+    <>
+      <Article 
+        title={"Reactの使い方"} 
+        // count={this.state.count}
+      />
+      <FooBar.Foo />
+      <FooBar.Bar />
+      <Hoge />
+    </>
+  )
 }
 
 export default Blog
